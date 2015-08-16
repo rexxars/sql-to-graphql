303f0e6de8c2881513ac41f4dbd3bd9ed49bf310
Espen Hovlandsdal <rexxars@gmail.com>
2015-08-16 16:13:42 +0200

    Don't split types if using relay

--- steps/output-data.js
+++ steps/output-data.js
@@ -25,36 +25,45 @@ function outputData(data, opts, callback) {
     var outputDir = path.resolve(opts.outputDir);
     var typesDir = path.join(outputDir, 'types');
     var configDir = path.join(outputDir, 'config');
-    mkdirp(typesDir, function(err) {
+    mkdirp(configDir, function(err) {
         if (err) {
             throw err;
         }
 
         // Write the configuration file
-        mkdirp(configDir, function(configErr) {
-            if (configErr) {
-                throw configErr;
-            }
+        var conf = recast.prettyPrint(buildConfig(opts), opts).code;
+        fs.writeFileSync(path.join(configDir, 'config.js'), conf);
 
-            var conf = recast.prettyPrint(buildConfig(opts), opts).code;
-            fs.writeFileSync(path.join(configDir, 'config.js'), conf);
-        });
+        // Relay-style or regular GraphQL?
+        if (opts.relay) {
+            // @todo Not splitting types into individual modules for relay because of
+            // circular dependencies + interfaces which are required at definition-type.
+            // Obviously we should find a solution to this and split stuff up.
+            var schema = recast.prettyPrint(buildProgram(data, opts), opts).code;
+            fs.writeFileSync(path.join(outputDir, 'schema.js'), schema);
+        } else {
+            mkdirp(typesDir, function(typesErr) {
+                if (typesErr) {
+                    throw typesErr;
+                }
+
+                // Write types
+                var type, ast, code;
+                for (type in data.types) {
+                    ast = buildType(data.types[type], opts);
+                    code = recast.prettyPrint(ast, opts).code;
+
+                    fs.writeFileSync(path.join(typesDir, data.types[type].varName + '.js'), code);
+                }
+
+                // Write the schema!
+                code = recast.prettyPrint(buildSchemaModule(data, opts), opts).code;
+                fs.writeFileSync(path.join(outputDir, 'schema.js'), code);
+            });
+        }
 
         // Copy templates ("static" ones, should probably be named something else)
         copyTemplates(opts.es6 ? 'es6' : 'cjs', outputDir);
-
-        // Write types
-        var type, ast, code;
-        for (type in data.types) {
-            ast = buildType(data.types[type], opts);
-            code = recast.prettyPrint(ast, opts).code;
-
-            fs.writeFileSync(path.join(typesDir, data.types[type].varName + '.js'), code);
-        }
-
-        // Write the all-important schema!
-        code = recast.prettyPrint(buildSchemaModule(data, opts), opts).code;
-        fs.writeFileSync(path.join(outputDir, 'schema.js'), code);
 
         callback();
     });
