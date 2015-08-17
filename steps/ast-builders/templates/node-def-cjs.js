4760d6baeba25706dc6268af3f64e97a7c009dab
Espen Hovlandsdal <rexxars@gmail.com>
2015-08-18 00:43:07 +0200

    Fix error in node definition code

--- steps/ast-builders/templates/node-def-cjs.js
+++ steps/ast-builders/templates/node-def-cjs.js
@@ -2,7 +2,7 @@ var node = nodeDefinitions(
     function(globalId, ast) {
         var gid = fromGlobalId(globalId);
         var fieldKind = gid.id.match(/^[0-9]+$/) ? 'IntValue' : 'StringValue';
-        var override = { arguments: [{ name: { value: 'id' }, value: { value: id, kind: fieldKind } }] };
+        var override = { arguments: [{ name: { value: 'id' }, value: { value: gid.id, kind: fieldKind } }] };
         return getEntityResolver(gid.type)(null, {}, ast, override);
     },
     function(data) {
