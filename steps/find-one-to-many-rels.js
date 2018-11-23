a497666153722230b53306b5592886bec496546a
johnsoftek <john.softek@gmail.com>
2018-11-24 07:24:40 +1000

    Multiple references working

--- steps/find-one-to-many-rels.js
+++ steps/find-one-to-many-rels.js
@@ -2,7 +2,6 @@
 
 var async = require('async');
 var after = require('lodash/after');
-var find = require('lodash/find');
 var camelCase = require('lodash/camelCase');
 var capitalize = require('lodash/capitalize');
 var pluralize = require('pluralize');
@@ -15,7 +14,10 @@ module.exports = function findOneToManyRelationships(adapter, models, callback) 
         return tasklist;
     }, {});
 
-    async.parallel(tasks, callback);
+    async.parallel(tasks, () => {
+      aliasMultipleReferences(models)
+      callback()
+    });
 };
 
 function findRelationships(adapter, model, models, callback) {
@@ -23,7 +25,7 @@ function findRelationships(adapter, model, models, callback) {
         return setImmediate(callback, null, model);
     }
 
-    var done = after(model.references.length, callback);
+    var done = after(model.references.length, callback)
     model.references.forEach(function(ref) {
         var referenceColumn = getUnaliasedField(ref.refField, model);
         adapter.hasDuplicateValues(model.table, referenceColumn, function(err, hasDupes) {
@@ -35,14 +37,15 @@ function findRelationships(adapter, model, models, callback) {
             //     return done(null, model);
             // }
             var reverseRefs = ref.model.listReferences;
-            var refName = camelCase(pluralize(model.name));
+            var refName = pluralize(model.name);
+            // var refName = camelCase(pluralize(model.name));
             var description = pluralize(model.name) + ' belonging to this ' + ref.model.name;
-            if (find(reverseRefs, { field: refName }) || ref.model.fields[refName]) {
-                // @TODO find a better name algo resolve mechanism
-                // `thread_id` should naturally be `threads`, while `old_thread_id` should be.. something else
-                refName += capitalize(camelCase(referenceColumn)).replace(/id$/i, '');
-                description += '..? (' + referenceColumn + ')';
-            }
+            // if (find(reverseRefs, { field: refName }) || ref.model.fields[refName]) {
+            //     // @TODO find a better name algo resolve mechanism
+            //     // `thread_id` should naturally be `threads`, while `old_thread_id` should be.. something else
+            //     refName += capitalize(ref.refField).replace(/id$/i, '');
+            //     description += '..? (' + referenceColumn + ')';
+            // }
 
             reverseRefs.push({
                 model: model,
@@ -65,3 +68,32 @@ function getUnaliasedField(field, model) {
     }
     return field
 }
+
+function aliasMultipleReferences(models) {
+    for (var type in models) {
+        aliasListReferences(models[type].listReferences)
+    }
+}
+
+
+function aliasListReferences(references) {
+  // If a model refers more than once to another model,
+  // we need to provide an alias field for additional
+  // references. Append the ref field.
+  let modelCnts = {}
+  references.forEach( ref => {
+    if (modelCnts[ref.field]) {
+      modelCnts[ref.field]++
+    } else {
+      modelCnts[ref.field] = 1
+    }
+  })
+  console.log('modelCnts', modelCnts)
+
+  references.forEach(ref => {
+    if (modelCnts[ref.field] > 1) {
+      ref.field = ref.field + capitalize(ref.refField).replace(/id$/i, '')
+    }
+  })
+}
+
